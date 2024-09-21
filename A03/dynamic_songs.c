b0159f6114ea7be30e3edb3d2fc448eda14ea091
apple <apple@wc116-dhcp-110.brynmawr.edu>
2024-09-20 21:55:13 -0400

    Finished A03 assignment: slow.c and dynamic_songs.c

--- A03/dynamic_songs.c
+++ A03/dynamic_songs.c
@@ -1,10 +1,72 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Yupei Sun
+ * Date: 2024-09-20
+ * Description: This program reads a CSV file containing song data,
+ *              dynamically stores the data in memory, and prints it.
  ---------------------------------------------*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char title[128];
+    char artist[128];
+    int duration;   // Duration in milliseconds
+    float danceability;
+    float energy;
+    float tempo;
+    float valence;
+} Song;
 
 int main() {
-  return 0;
+    FILE *file = fopen("songlist.csv", "r");
+    if (!file) {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    char buffer[256];
+    int num_songs;
+
+    // Read the first line to get the number of songs
+    fgets(buffer, sizeof(buffer), file);
+    num_songs = atoi(strtok(buffer, ","));  // Get the number of songs from the first token
+
+    // Dynamically allocate memory for the array of songs
+    Song *songs = (Song *)malloc(num_songs * sizeof(Song));
+    if (!songs) {
+        printf("Memory allocation failed\n");
+        fclose(file);
+        return 1;
+    }
+
+    // Skip the header line
+    fgets(buffer, sizeof(buffer), file);
+
+    // Read and store each song's information
+    for (int i = 0; i < num_songs; i++) {
+        fgets(buffer, sizeof(buffer), file);
+        strcpy(songs[i].title, strtok(buffer, ","));
+        strcpy(songs[i].artist, strtok(NULL, ","));
+        songs[i].duration = atoi(strtok(NULL, ","));
+        songs[i].danceability = atof(strtok(NULL, ","));
+        songs[i].energy = atof(strtok(NULL, ","));
+        songs[i].tempo = atof(strtok(NULL, ","));
+        songs[i].valence = atof(strtok(NULL, ","));
+    }
+
+    // Print out the song list
+    printf("Welcome to Dynamic Donna's Danceability Directory.\n");
+    for (int i = 0; i < num_songs; i++) {
+        printf("%d) %s by %s\n   Duration: %d ms, Danceability: %.3f, Energy: %.3f, Tempo: %.3f, Valence: %.3f\n",
+               i, songs[i].title, songs[i].artist, songs[i].duration, songs[i].danceability, songs[i].energy,
+               songs[i].tempo, songs[i].valence);
+    }
+
+    // Free the dynamically allocated memory
+    free(songs);
+    fclose(file);
+
+    return 0;
 }
--- A03/slow.c
+++ A03/slow.c
@@ -1,10 +1,48 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Yupei Sun
+ * Date: 2024-09-20
+ * Description: It will implement the dynamic memory allocation
+ *              and handle song data in C using malloc and free.
  ---------------------------------------------*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-  return 0;
+    char buff[32];
+    int pause_len;
+    
+    // Ask user for the pause length
+    printf("Pause length: ");
+    scanf("%d", &pause_len);
+    
+    // Ask user for the text input
+    printf("Text: ");
+    scanf("%s", buff);  // Read input text
+
+    // Calculate the required space for the output string
+    int new_len = strlen(buff) * (pause_len + 1);
+    char *ent_phrase = (char *)malloc(new_len + 1);  // Dynamically allocate space
+
+    // Convert the phrase to ent-speech
+    int idx = 0;
+    for (int i = 0; i < strlen(buff); i++) {
+        ent_phrase[idx++] = buff[i];
+        // Add dots after every character, including punctuation, except for the very last character
+        if (i != strlen(buff) - 1) {
+            for (int j = 0; j < pause_len; j++) {
+                ent_phrase[idx++] = '.';
+            }
+        }
+    }
+    ent_phrase[idx] = '\0';  // Add null terminator at the end of the string
+
+    // Output the ent-speech version of the text
+    printf("%s\n", ent_phrase);
+    
+    // Free the dynamically allocated memory
+    free(ent_phrase);
+
+    return 0;
 }
