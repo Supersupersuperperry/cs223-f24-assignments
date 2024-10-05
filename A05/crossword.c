39939ec1997c36ad924f4a9378f513410d081433
apple <apple@wc120-dhcp-104.brynmawr.edu>
2024-10-05 02:09:10 -0400

    finished

--- A05/crossword.c
+++ A05/crossword.c
@@ -1,10 +1,82 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Your Name
+ * Date: 2024-10-04
+ * Description: 
+ * This program takes two words as input from the command line and 
+ * generates a crossword puzzle by identifying a common letter 
+ * between them. One word is placed vertically and the other horizontally.
+ * If no common letter is found, the program will print an error message.
  ---------------------------------------------*/
-#include <stdio.h>
 
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+  // Check if the correct number of arguments is provided
+  if (argc < 3) {
+    printf("Usage: %s <word1> <word2>\n", argv[0]);
+    return 1;
+  }
+
+  // Get the words from command line arguments
+  char* word1 = argv[1];
+  char* word2 = argv[2];
+
+  // Find the common letter between the two words
+  int len1 = strlen(word1);
+  int len2 = strlen(word2);
+  int common_index1 = -1, common_index2 = -1;
+
+  for (int i = 0; i < len1; i++) {
+    for (int j = 0; j < len2; j++) {
+      if (word1[i] == word2[j]) {
+        common_index1 = i;
+        common_index2 = j;
+        break;
+      }
+    }
+    if (common_index1 != -1) break;
+  }
+
+  // If no common letter is found, print a message and exit
+  if (common_index1 == -1) {
+    printf("No common letter!\n");
+    return 0;
+  }
+
+  // Dynamically allocate memory for the crossword grid
+  int rows = len2;
+  int cols = len1;
+  char** crossword = (char**) malloc(rows * sizeof(char*));
+  for (int i = 0; i < rows; i++) {
+    crossword[i] = (char*) malloc(cols * sizeof(char));
+    for (int j = 0; j < cols; j++) {
+      crossword[i][j] = '.';  // Fill the grid with dots
+    }
+  }
+
+  // Place word1 horizontally and word2 vertically in the crossword
+  for (int i = 0; i < len1; i++) {
+    crossword[common_index2][i] = word1[i];
+  }
+  for (int j = 0; j < len2; j++) {
+    crossword[j][common_index1] = word2[j];
+  }
+
+  // Print the crossword
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      printf("%c ", crossword[i][j]);
+    }
+    printf("\n");
+  }
+
+  // Free the allocated memory
+  for (int i = 0; i < rows; i++) {
+    free(crossword[i]);
+  }
+  free(crossword);
+
   return 0;
 }
--- A05/write_ppm.c
+++ A05/write_ppm.c
@@ -1,18 +1,27 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Yupei Sun    
+ * Date: Oct 4, 2024
+ * Description: This function writes pixel data to a binary PPM (P6) file. It 
+ * writes the image header (width, height, max color value) and 
+ * then writes the RGB pixel data to the file.
  ---------------------------------------------*/
 #include "write_ppm.h"
 #include <stdio.h>
 #include <string.h>
 
-// implement *one* (but do not remove the other one)!
-
 void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+    FILE* fp = fopen(filename, "wb");
+    if (!fp) {
+        fprintf(stderr, "Error: could not open file %s for writing\n", filename);
+        return;
+    }
 
-}
+    // Write the header: magic number, width, height, and max color value
+    fprintf(fp, "P6\n%d %d\n255\n", w, h);
 
-void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
+    // Write pixel data
+    fwrite(pixels, sizeof(struct ppm_pixel), w * h, fp);
 
+    // Close the file
+    fclose(fp);
 }
